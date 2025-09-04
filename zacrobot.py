f69954b825f9762e9255bf93edc7a0800c2747f4
Adithya-Lakkugit config --global user.email adithya.lakku@gmail.com <adithya.lakku@gmail.com>
2025-09-04 15:28:57 +0530

    added docker part mostly necessary

---
+++ zacrobot.py
@@ -0,0 +1,110 @@
+import discord
+import requests
+import json
+from PyPDF2 import PdfReader
+import os
+import asyncio
+from keys import DiscordBotToken,DiscordWebhookUrl,OpenRouterToken,TOKEN,ChannelID,MODEL
+
+def readPdf(filepath):
+    reader = PdfReader(filepath) 
+    #print(len(reader.pages))
+
+    text = ''
+
+    # Read text from each page
+    for page in reader.pages:
+        text = text + page.extract_text()
+
+    #print(text)
+
+    return text
+
+
+def dummytester():
+    dummytext = """ yo what up this is a test"""
+
+    response = requests.post(DiscordWebhookUrl,json = {"content":dummytext})
+    if response.status_code==204:
+        print("discord success")
+    else:
+        print("discord fail")
+
+
+def Summarizerv2(text):
+    
+    url = "http://ollama:11434/api/generate"
+
+
+    #prompt = f"summarize the text in 300 characters {text}"
+
+    payload = {
+    "model": "gemma3:4b",
+    "prompt": f"summarize the text in 300 characters {text}",
+    "stream": False
+}
+
+    response = requests.post(url,json=payload,stream=False)
+
+    data = response.json()
+    
+    try:
+
+        if response.status_code == 200:
+            print("ai success")
+            if "message" in data and "content" in data["message"]:
+                output = data["message"]["content"]
+                print(output)
+                return output
+        else:
+            print("ai fail")
+    except json.JSONDecodeError:
+        print("shit failed")
+
+
+
+
+def post_to_discord(text):
+
+    
+    response = requests.post(DiscordWebhookUrl,json={"content": text})
+
+    if response.status_code == 204:
+        print(" discord success")
+    else :
+        print("discord fail")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'✅ Logged in as {client.user}')
+    dummytester()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    for attachment in message.attachments:
+        print(f"📄 PDF received: {attachment.filename}")
+
+        #  Download the PDF to your local PC
+        file_path = os.path.join("downloads", attachment.filename)
+        os.makedirs("downloads", exist_ok=True)
+        await attachment.save(file_path)
+        
+        text = readPdf(filepath=file_path)
+        #text1 = Summarizer(text)
+        text1 = Summarizerv2(text)
+        
+        post_to_discord(text1)
+        
+
+        print(f"💾 Saved to {file_path}")
+        break
+
+client.run(DiscordBotToken)
